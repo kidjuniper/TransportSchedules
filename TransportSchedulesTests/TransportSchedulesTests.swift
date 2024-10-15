85d01fe1a1e972db4b1716899bc50db356517b7b
kidjuniper <kidjuniper@yandex.ru>
2024-10-15 12:23:22 +0300

    Initial Commit

---
+++ TransportSchedulesTests/TransportSchedulesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TransportSchedulesTests.swift
+//  TransportSchedulesTests
+//
+//  Created by Nikita Stepanov on 15.10.2024.
+//
+
+import Testing
+@testable import TransportSchedules
+
+struct TransportSchedulesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
